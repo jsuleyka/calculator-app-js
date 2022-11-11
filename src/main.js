fb7cd7e7235f3a6400e41991e344169935676268
jsuleyka <suleymjb15@hotmail.com>
2022-11-11 11:13:02 -0600

    mobile design

---
+++ src/main.js
@@ -0,0 +1,21 @@
+const screen = document.getElementById('screen');
+const reset = document.getElementById('reset');
+const equal = document.getElementById('equal');
+
+const sum = document.getElementById('sum');
+const sub = document.getElementById('sub');
+const mult = document.getElementById('mult');
+const div = document.getElementById('div');
+
+const one = document.getElementById('one').value;
+const two = document.getElementById('two');
+const three = document.getElementById('three');
+const four = document.getElementById('four');
+const five = document.getElementById('five');
+const six = document.getElementById('six');
+const seven = document.getElementById('seven');
+const eight = document.getElementById('eight');
+const nine = document.getElementById('nine');
+const zero = document.getElementById('zero');
+
+console.log(one);
